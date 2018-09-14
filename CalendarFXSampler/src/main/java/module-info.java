87a312a2aa7e6af4610f36b0b2e3f4a50c88aaf8
Dirk Lemmermann <dlemmermann@gmail.com>
2018-09-14 16:34:36 +0200

    Added module-info.java to view, recurrence, app, and sampler modules.

---
+++ CalendarFXSampler/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+import com.calendarfx.demo.CalendarFXSamplerProject;
+
+module com.calendarfx.sampler {
+    requires com.calendarfx.view;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.web;
+    requires controlsfx;
+    requires fxsampler;
+
+    exports com.calendarfx.demo;
+    exports com.calendarfx.demo.entries;
+    exports com.calendarfx.demo.pages;
+    exports com.calendarfx.demo.performance;
+    exports com.calendarfx.demo.popover;
+    exports com.calendarfx.demo.print;
+    exports com.calendarfx.demo.views;
+
+    provides fxsampler.FXSamplerProject with CalendarFXSamplerProject;
+}
